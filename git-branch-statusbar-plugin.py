b7055c8162b2917045508213c01fba7452f00ad4
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2019-03-28 19:11:00 -0700

    Updated copyright line

--- git-branch-statusbar-plugin.py
+++ git-branch-statusbar-plugin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #    Gedit Git Branch On Statusbar Plugin
-#    Copyright (C) 2010-2011 Denis Fuenzalida <denis.fuenzalida@gmail.com>
+#    Copyright (C) 2010-2019 Denis Fuenzalida <denis.fuenzalida@gmail.com>
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
